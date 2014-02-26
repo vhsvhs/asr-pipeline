7ef8ceb757a601146bc7447c99a27dbf39ccbcc0
vhsvhs <victorhansonsmith@gmail.com>
2014-02-26 14:01:40 -0800

    More work on the Ubuntu setup script.

--- setup/setup.ubuntu.sh
+++ setup/setup.ubuntu.sh
@@ -1,7 +1,9 @@
+##############################################################
 #
 # This shell script will install all the dependent software
 # necessary to run the ASR Pipeline
 #
+##############################################################
 wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py -O - | python
 sudo easy_install -U dendropy
 sudo apt-get install python-biopython
@@ -9,6 +11,7 @@ sudo apt-get install python-biopython
 mkdir common
 
 # MUSCLE
+####################
 curl -O http://drive5.com/muscle/downloads3.6/muscle3.6_src.tar.gz
 mv muscle3.6_src.tar.gz /common/
 cd /common
@@ -19,7 +22,8 @@ make
 ln -s /common/muscle3.6_src/muscle  /bin/muscle
 cd /
 
-#MSAPROBS
+# MSAPROBS
+###############
 cp ~/asr-pipeline/apps/MSAProbs-0.9.7.tar /common/
 cd /common
 tar -xvf MSAProbs-0.9.7.tar 
@@ -28,12 +32,31 @@ make
 ln -s /common/MSAProbs-0.9.7/MSAProbs/msaprobs /bin/msaprobs
 cd /
 
-#PHYML
+# PHYML
+################
 #curl -O https://github.com/stephaneguindon/phyml-downloads/releases/download/stable/phyml-20120412.tar.gz
 #mv phyml-20120412.tar.gz /common/
 cp ~/asr-pipeline/apps/phyml-20120412.tar /common/
 cd /common
 #gzip -d phyml-20120412.tar.gz
 tar -xvf phyml-20120412.tar
+cd phyml-20120412
+./configure
+make
+ln -s /common/phyml-20120412/src/phyml /bin/phyml
+cd /
+
+#
+# RAxML
+#
+cd /common
+git clone git://github.com/stamatak/standard-RAxML.git
+cd standard-RAxML/
+make -f Makefile.PTHREADS.gcc
+ln -s /common/standard-RAxML/raxmlHPC-PTHREADS /bin/raxml
+
+# NUMPY and SCIPY
+apt-get install python-numpy
+apt-get install python-scipy
 
 
