433e26ba378f4fc50b67811bf675921dafdff811
vhsvhs <victorhansonsmith@gmail.com>
2015-02-03 09:50:41 -0800

    More work on the Pythoscape interface

--- pythoscape_tools.py
+++ pythoscape_tools.py
@@ -39,66 +39,56 @@ def run_similarity_network_analysis(con):
     fpath = "pythoscape/pythoscape_sequences.fasta"
     write_fasta(sequences, fpath)
     
-
+    
     my_interface = l_i.LocalInterface("pythoscape")
     my_pytho = env.PythoscapeEnvironment(my_interface)
     
     #Create plug-ins
     
+    #print dir(my_pytho)
+    
+    #print dir(my_pytho.graph_interface)
+    
     #Import sequences
     #plugin_1 = i_s.ImportFromFastaFile(fpath, id_re=RE_PATTERNS['UniprotKB'], id_name='Uniprot')
-    plugin_1 = i_seq.ImportFromFastaFile(fpath, flat_id=True, id_name='erg')
-    
-    #Execute plug-ins
-    my_pytho.execute_plugin(plugin_1)
-    
-    #Input edges to Pythoscape
+    plugin_1 = i_seq.ImportFromFastaFile(fpath, id_name='Uniprot')
+
     blastp_exe = get_setting_values(con, "blastp_exe")
     if blastp_exe == None:
         write_log(con, "I can't find your BLAST executable. I'm skipping the similarity network analysis.")
         return
     blastp_exe = blastp_exe[0]
-    plugin_2 = a_l_b.AddBLASTEdgesFromLocalBLAST(blastp_exe, include_atts=['-log10(E)'])
-    
-    #Execute plug-ins
-    my_pytho.execute_plugin(plugin_2)
-    
-    # Skipping plugin 3 (UniProt annotations)
-    
-    #Create representative network
+    plugin_2 = a_l_b.AddBLASTEdgesFromLocalBLAST("blastp", include_atts=['-log10(E)'])
     cdhit_exe = get_setting_values(con, "cdhit_exe")
     if cdhit_exe == None:
         write_log(con, "I canot find your CD-HIT executable. I'm skipping the similarity network analysis.")
         return
+    
     cdhit_exe = cdhit_exe[0]
-    plugin_4 = m_c_r.CreateCDHITRepnodes('rep-net', cdhit_exe, c=0.7, n=5)
-    
-    #Execute plug-ins
-    my_pytho.execute_plugin(plugin_4)
+    repnode_att = "cdhit 0.7"
+    network_name = "rep-net"
+    plugin_4 = m_c_r.CreateCDHITRepnodes(network_name, "cd-hit", c=0.7, n=5, repnode_att=repnode_att)
 
-    #Create representative network
-    plugin_5 = r_s.CalcNodeSize('rep-net','cdhit 0.70','node size')
-    
-    #Execute plug-ins
-    my_pytho.execute_plugin(plugin_5)
-    
-    #Create representative network
-    plugin_6 = a_r_e.AddEdgesToRepnodeNetwork('rep-net','cdhit 0.70','-log10(E)',filt_dir='>',filt_value=0.0)
-    
-    #Execute plug-ins
-    my_pytho.execute_plugin(plugin_6)
-
-    #Create representative node attributes
-    plugin_7 = a_r_a.AddAttributesByIfAny('rep-net','cdhit 0.70','family','repnode family')
-    
-    #Execute plug-ins
-    my_pytho.execute_plugin(plugin_7)
-    
-    #Output network
+    plugin_5 = r_s.CalcNodeSize(network_name,repnode_att,'node size')
+    plugin_6 = a_r_e.AddEdgesToRepnodeNetwork(network_name,repnode_att,'-log10(E)',filt_dir='>',filt_value=0.0)
+    plugin_7 = a_r_a.AddAttributesByIfAny(network_name,repnode_att,'family','repnode family')
     out_plugin_1 = o_x.OutputXGMML('gst-sigma-rep-net-20.xgmml','sigma gst representative node network @ 20',filt_name='rep-net mean',filt_dir='>',filt_value=20)
     
+    
     #Execute plug-ins
-    my_net = env.PythoscapeNetwork('rep-net',my_interface)
+    my_pytho.execute_plugin(plugin_1)
+    my_pytho.execute_plugin(plugin_2)
+    edge_query = my_interface.Edge()
+    print "70:", len([True for edge in my_interface.pull(edge_query)])
+    
+    #Create representative network
+    my_pytho.execute_plugin(plugin_4)
+    my_pytho.execute_plugin(plugin_5)
+    my_pytho.execute_plugin(plugin_6)
+    my_pytho.execute_plugin(plugin_7)
+    
+    #Execute plug-ins
+    my_net = env.PythoscapeNetwork(network_name,my_interface)
     my_net.execute_plugin(out_plugin_1)
 
 
